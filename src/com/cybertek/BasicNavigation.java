5faab4f2cc7472559beecc9bb52bf8cddd918f69
Jovidon <you@example.comjova_94@inbox.ru>
2019-06-16 15:34:28 -0700

    Hello Guys

--- src/com/cybertek/BasicNavigation.java
+++ src/com/cybertek/BasicNavigation.java
@@ -4,5 +4,7 @@ public class BasicNavigation {
 
     public static void main(String[] args) {
         System.out.println("Hello Im new class");
+
+        System.out.println("Hello Hojam");
     }
 }
